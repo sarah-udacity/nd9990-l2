45e7c0b3197d4ef981b4926c0034293ad7992db7
Sarah Maris <sarah.maris@udacity.com>
2021-06-10 14:42:55 -0400

    Update server port to 3000 for Udacity workspace

--- src/server.ts
+++ src/server.ts
@@ -4,69 +4,60 @@ import bodyParser from 'body-parser';
 import { Car, cars as cars_list } from './cars';
 
 (async () => {
-  let cars:Car[]  = cars_list;
+  let cars: Car[] = cars_list;
 
   //Create an express applicaiton
-  const app = express(); 
+  const app = express();
   //default port to listen
-  const port = 8082; 
-  
-  //use middleware so post bodies 
+  const port = 3000;
+
+  //use middleware so post bodies
   //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+  app.use(bodyParser.json());
 
   // Root URI call
-  app.get( "/", ( req: Request, res: Response ) => {
-    res.status(200).send("Welcome to the Cloud!");
-  } );
+  app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Welcome to the Cloud!');
+  });
 
-  // Get a greeting to a specific person 
+  // Get a greeting to a specific person
   // to demonstrate routing parameters
   // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
-    ( req: Request, res: Response ) => {
-      let { name } = req.params;
+  app.get('/persons/:name', (req: Request, res: Response) => {
+    let { name } = req.params;
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+    if (!name) {
+      return res.status(400).send(`name is required`);
+    }
 
-      return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+    return res.status(200).send(`Welcome to the Cloud, ${name}!`);
+  });
 
   // Get a greeting to a specific person to demonstrate req.query
   // > try it {{host}}/persons?name=the_name
-  app.get( "/persons/", ( req: Request, res: Response ) => {
+  app.get('/persons/', (req: Request, res: Response) => {
     let { name } = req.query;
 
-    if ( !name ) {
-      return res.status(400)
-                .send(`name is required`);
+    if (!name) {
+      return res.status(400).send(`name is required`);
     }
 
-    return res.status(200)
-              .send(`Welcome to the Cloud, ${name}!`);
-  } );
+    return res.status(200).send(`Welcome to the Cloud, ${name}!`);
+  });
 
   // Post a greeting to a specific person
   // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as 
+  // > try it by posting {"name": "the_name" } as
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
-    async ( req: Request, res: Response ) => {
+  app.post('/persons', async (req: Request, res: Response) => {
+    const { name } = req.body;
 
-      const { name } = req.body;
+    if (!name) {
+      return res.status(400).send(`name is required`);
+    }
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
-
-      return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+    return res.status(200).send(`Welcome to the Cloud, ${name}!`);
+  });
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
@@ -79,8 +70,8 @@ import { Car, cars as cars_list } from './cars';
   // it should require id, type, model, and cost
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
